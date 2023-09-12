6336afd07e65da9682162bf0109e99c8a97db91f
chris-d <chrisdennis117@gmail.com>
2023-09-12 10:31:55 -0700

    ignore target

--- dungeon-escape/src/main.rs
+++ dungeon-escape/src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, world!");
+use rltk::{Rltk, GameState, RGB, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max, min};
+use specs_derive::Component;
+
+// macro for getting boilerplate for component x
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable{
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
+
+struct State {
+    ecs: World
+
+}
+impl GameState for State {
+    fn tick(&mut self, ctx : &mut Rltk) {
+        ctx.cls();
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+        for (pos, render) in (&positions, &renderables).join(){
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+    let mut gs = State { ecs: World::new()};
+
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+
+    gs.ecs.create_entity().with(Position{x: 25, y:25}).with(Renderable{
+        glyph: rltk::to_cp437('#'),
+        fg: RGB::named(rltk::RED),
+        bg: RGB::named(rltk::GRAY),
+    }).build();
+
+    rltk::main_loop(context, gs)
+
 }
